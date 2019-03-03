606530774cb095fc096d877798900660fb6c106a
midghaar <anton_molgan@hotmail.com>
2019-03-03 14:04:53 +0100

    Updates

---
+++ precache-manifest.597edab3b4970b0cc3b076749118d1bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "878a1452dac051494898",
+    "url": "/AddressitudeReact/static/css/main.7185aadb.chunk.css"
+  },
+  {
+    "revision": "878a1452dac051494898",
+    "url": "/AddressitudeReact/static/js/main.878a1452.chunk.js"
+  },
+  {
+    "revision": "db5fe01e468e10eaa205",
+    "url": "/AddressitudeReact/static/js/runtime~main.db5fe01e.js"
+  },
+  {
+    "revision": "d3f21a2eca00cceb4fcc",
+    "url": "/AddressitudeReact/static/js/2.d3f21a2e.chunk.js"
+  },
+  {
+    "revision": "429b9f13916d1ae602d4",
+    "url": "/AddressitudeReact/static/css/3.d24ee9ca.chunk.css"
+  },
+  {
+    "revision": "429b9f13916d1ae602d4",
+    "url": "/AddressitudeReact/static/js/3.429b9f13.chunk.js"
+  },
+  {
+    "revision": "15e23a2ebfe45a8cb313",
+    "url": "/AddressitudeReact/static/js/4.15e23a2e.chunk.js"
+  },
+  {
+    "revision": "ec228ef4d5df99344f1c0e425ba1bd1a",
+    "url": "/AddressitudeReact/static/media/loader.ec228ef4.svg"
+  },
+  {
+    "revision": "c519ca80f24cb31501521586936c38a0",
+    "url": "/AddressitudeReact/index.html"
+  }
+];
